4bcd5bfe91153751c952fceb43e015d76a56bd25
Kolade Adewuyi <koladeadewuyi@users.noreply.github.com>
2024-05-15 10:36:04 +0100

    DAC6-2811: Add address-non-uk page object

---
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/FiAddressNonUK.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.test.ui.pages
+
+import org.openqa.selenium.By
+
+object FiAddressNonUK extends BasePage {
+
+  override val pageUrl: String = baseUrl + "/address-non-uk"
+  val addressLine1: By         = By.id("addressLine1")
+  val addressLine2: By         = By.id("addressLine2")
+  val city: By                 = By.id("addressLine3")
+  val region: By               = By.id("addressLine4")
+  val postCode: By             = By.id("postCode")
+  val country: By              = By.id("country")
+  val countryOption: By        = By.id("country__option--0")
+
+  def enterAddressManually(): Unit = {
+    onPage(pageUrl)
+    sendTextById(addressLine1, "addressLine1")
+    sendTextById(addressLine2, "addressLine2")
+    sendTextById(city, "Some City")
+    sendTextById(region, "Some Region")
+    sendTextById(postCode, "A12 BC4")
+    driver.findElement(country).click()
+    driver.findElement(country).sendKeys("Andorra")
+    driver.findElement(countryOption).click()
+    submitPageById()
+  }
+}
